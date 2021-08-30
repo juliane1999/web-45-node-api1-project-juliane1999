b64294445e8c9988948162fb1b98d3568a922b1b
juliane1999 <lastjuliane@gmail.com>
2021-08-30 16:36:36 -0700

    passed one more test

--- api/server.js
+++ api/server.js
@@ -8,21 +8,20 @@ server.get('/', (req, res) => {
     res.status(200).json({ message: 'here' })
   })
 
-  
-  server.post('/api/users', (req, res) => {
-      const newUser = req.body
-    User.insert(newUser)
-    .then(user => {
-        if (!newUser) {
-            res.status(400).json({ message: "Please provide name and bio for the user" })
-        } else {
-            res.status(201).json(user)
-        }
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ message: "There was an error while saving the user to the database" })
-    })
+  server.post('/api/users', (req,res)=> {
+    const newUser = req.body
+    if(!newUser.name || !newUser.bio){
+      res.status(400).json({ message: "Please provide name and bio for the user" })
+    }else{
+      User.insert(newUser)
+      .then(user => {
+        res.status(201).json(user)
+      })
+      .catch(err=>{
+        console.log(err)
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+      })
+    }
   })
 
   server.get('/api/users/:id', (req, res) => {
@@ -59,21 +58,18 @@ server.get('/', (req, res) => {
     const changes = req.body
     console.log(id, changes)
     try {
+        const result = await User.update(id, changes)
+        res.status(200).json(result)
+      } catch (err) {
       if (!id) {
         res.status(404).json( {message: "The user with the specified ID does not exist" })
         } else if (!changes) {
           res.status(400).json({ message: "Please provide name and bio for the user" })
-        } else {
-          const result = await User.update(id, changes)
-          res.status(200).json(result)
-        }
-      } catch (err) {
-        console.log(err)
+        }else{
         res.status(500).json({ message: "The user information could not be modified" })
       }
+    }
   })
-
-      
 
 
   server.delete('/api/users/:id', (req, res) => {
@@ -90,10 +86,6 @@ server.get('/', (req, res) => {
         res.status(500).json({ message: "The user could not be removed" })
       })
   })
-
-
-
-
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
