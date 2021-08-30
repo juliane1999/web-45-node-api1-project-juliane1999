2d01235896afd601f21cabca14d44fe132b83092
juliane1999 <lastjuliane@gmail.com>
2021-08-30 15:36:46 -0700

    passed tests, 3 left

--- api/server.js
+++ api/server.js
@@ -12,11 +12,11 @@ server.get('/', (req, res) => {
   server.post('/api/users', (req, res) => {
       const newUser = req.body
     User.insert(newUser)
-    .then(newUser => {
+    .then(user => {
         if (!newUser) {
             res.status(400).json({ message: "Please provide name and bio for the user" })
         } else {
-            res.status(201).json(newUser)
+            res.status(201).json(user)
         }
     })
     .catch(err => {
@@ -59,13 +59,22 @@ server.get('/', (req, res) => {
     const changes = req.body
     console.log(id, changes)
     try {
-        const result = await User.update(id, changes)
-        res.status(200).json(result)
+      if (!id) {
+        res.status(404).json( {message: "The user with the specified ID does not exist" })
+        } else if (!changes) {
+          res.status(400).json({ message: "Please provide name and bio for the user" })
+        } else {
+          const result = await User.update(id, changes)
+          res.status(200).json(result)
+        }
       } catch (err) {
         console.log(err)
         res.status(500).json({ message: "The user information could not be modified" })
       }
   })
+
+      
+
 
   server.delete('/api/users/:id', (req, res) => {
     User.remove(req.params.id)
